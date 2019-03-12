3ccbf6474b31d8b3b0874d7c543958720df866e9
carlosmantilla23 <carlos.mantilla.jaimes@gmail.com>
2019-03-11 22:17:41 -0500

    CRUD básico

--- src/main/java/com/carlosmantilla/profesoresplatzi/dao/AbstractSession.java
+++ src/main/java/com/carlosmantilla/profesoresplatzi/dao/AbstractSession.java
@@ -1,5 +1,18 @@
 package com.carlosmantilla.profesoresplatzi.dao;
 
-public abstract class AbstractSession {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
+public abstract class AbstractSession {
+	
+	//una clase abstracta la idea es que cuando se ejecute la herencia sea obligatorio sobreescribir o traer los métodos
+
+	
+	@Autowired //Con Autowired ve y encuentra la instanciacón y persistelo aquí
+	private SessionFactory sessionFactory;
+	
+	protected Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
 }
--- src/main/java/com/carlosmantilla/profesoresplatzi/dao/TeacherDAOImpl.java
+++ src/main/java/com/carlosmantilla/profesoresplatzi/dao/TeacherDAOImpl.java
@@ -4,37 +4,44 @@ import java.util.List;
 
 import com.carlosmantilla.profesoresplatzi.model.Teacher;
 
-public class TeacherDAOImpl implements TeacherDAO {
+public class TeacherDAOImpl extends AbstractSession implements TeacherDAO {
 
 	public void saveTeacher(Teacher teacher) {
 		// TODO Auto-generated method stub
+		getSession().persist(teacher); //
 
 	}
 
 	public List<Teacher> findAllTeachers() {
 		// TODO Auto-generated method stub
-		return null;
+		return getSession().createQuery("from Teacher").list();
+
 	}
 
 	public void updateTeacher(Teacher teacher) {
 		// TODO Auto-generated method stub
+		getSession().update(teacher);
 
 	}
 
 	public void deleteTeacherById(Long idTeacher) {
 		// TODO Auto-generated method stub
-
+		Teacher teacher = findById(idTeacher);
+		if (teacher != null) {
+			getSession().delete(teacher);
+		}
 	}
 
 	public Teacher findById(Long idTeacher) {
 		// TODO Auto-generated method stub
-		return null;
+		return (Teacher) getSession().get(Teacher.class, idTeacher);
 
 	}
 
 	public Teacher findByName(String name) {
 		// TODO Auto-generated method stub
-		return null;
+		return (Teacher) getSession().createQuery("FROM Teacher WHERE name = :name").setParameter("name", name)
+				.uniqueResult();
 	}
 
 }
